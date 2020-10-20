81a0ab764dac9a81128f7505a2f44871c1fc607c
Excelsies <gme97531@gmail.com>
2020-10-20 09:53:00 -0600

    Added the recipes
    Set up each recipe and added an event to attach the metadata

--- MultiArmor/multiarmor/src/main/me/excelsies/multiarmor/Main.java
+++ MultiArmor/multiarmor/src/main/me/excelsies/multiarmor/Main.java
@@ -1,16 +1,153 @@
 package me.excelsies.multiarmor;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.SmithingRecipe;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapedRecipe;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public class Main extends JavaPlugin 
+
+public class Main extends JavaPlugin
 {
     @Override
     public void onEnable(){
-        getLogger().info("Hello, SpigotMC!");
+        Bukkit.addRecipe(LeatherElytra());
+        Bukkit.addRecipe(ChainElytra());
+        Bukkit.addRecipe(GoldElytra());
+        Bukkit.addRecipe(IronElytra());
+        Bukkit.addRecipe(DiamondElytra());
+        Bukkit.addRecipe(NetheriteElytra());
+        Bukkit.addRecipe(NautilusRecipe());
+
+        getServer().getPluginManager().registerEvents(new OnSmithingListener(), this);
+
+        getLogger().info(ChatColor.GREEN + "[MultiArmor]: Plugin is enabled!");
     }
 
     @Override
     public void onDisable(){
-        getLogger().info("See you again, SpigotMc!");
+        getLogger().info(ChatColor.RED + "[MultiArmor]: Plugin is disabled!");
     }
+
+    
+
+    public SmithingRecipe LeatherElytra(){
+        
+        ItemStack result = new ItemStack(Material.ELYTRA);
+        
+        RecipeChoice base = new RecipeChoice.MaterialChoice(Material.LEATHER_CHESTPLATE);
+        RecipeChoice addition = new RecipeChoice.MaterialChoice(Material.ELYTRA);
+
+        NamespacedKey key = new NamespacedKey(this, "Leather_Armored_Elytra");
+
+        SmithingRecipe recipe = new SmithingRecipe(key, result, base, addition);
+        
+        
+        return recipe;
+    }
+
+    public SmithingRecipe ChainElytra(){
+        
+        ItemStack result = new ItemStack(Material.ELYTRA);
+        
+        RecipeChoice base = new RecipeChoice.MaterialChoice(Material.CHAINMAIL_CHESTPLATE);
+        RecipeChoice addition = new RecipeChoice.MaterialChoice(Material.ELYTRA);
+        
+        NamespacedKey key = new NamespacedKey(this, "Chainmail_Armored_Elytra");
+
+        SmithingRecipe recipe = new SmithingRecipe(key, result, base, addition);
+
+        return recipe;
+    }
+
+    public SmithingRecipe IronElytra(){
+        
+        ItemStack result = new ItemStack(Material.ELYTRA);
+        
+        RecipeChoice base = new RecipeChoice.MaterialChoice(Material.IRON_CHESTPLATE);
+        RecipeChoice addition = new RecipeChoice.MaterialChoice(Material.ELYTRA);
+        
+        NamespacedKey key = new NamespacedKey(this, "Iron_Armored_Elytra");
+
+        SmithingRecipe recipe = new SmithingRecipe(key, result, base, addition);
+
+        return recipe;
+    }
+
+    
+    public SmithingRecipe GoldElytra(){
+        
+        ItemStack result = new ItemStack(Material.ELYTRA);
+        
+        RecipeChoice base = new RecipeChoice.MaterialChoice(Material.GOLDEN_CHESTPLATE);
+        RecipeChoice addition = new RecipeChoice.MaterialChoice(Material.ELYTRA);
+        
+        NamespacedKey key = new NamespacedKey(this, "Gold_Armored_Elytra");
+
+        SmithingRecipe recipe = new SmithingRecipe(key, result, base, addition);
+
+        return recipe;
+    }
+
+    public SmithingRecipe DiamondElytra(){
+        
+        ItemStack result = new ItemStack(Material.ELYTRA);
+        
+        RecipeChoice base = new RecipeChoice.MaterialChoice(Material.DIAMOND_CHESTPLATE);
+        RecipeChoice addition = new RecipeChoice.MaterialChoice(Material.ELYTRA);
+        
+        NamespacedKey key = new NamespacedKey(this, "Diamond_Armored_Elytra");
+
+        SmithingRecipe recipe = new SmithingRecipe(key, result, base, addition);
+
+        return recipe;
+    }
+
+    public SmithingRecipe NetheriteElytra(){
+        
+        ItemStack result = new ItemStack(Material.ELYTRA);
+        RecipeChoice base = new RecipeChoice.MaterialChoice(Material.NETHERITE_CHESTPLATE);
+        RecipeChoice addition = new RecipeChoice.MaterialChoice(Material.ELYTRA);
+        
+        NamespacedKey key = new NamespacedKey(this, "Netherite_Armored_Elytra");
+
+        SmithingRecipe recipe = new SmithingRecipe(key, result, base, addition);
+
+        return recipe;
+    } 
+
+    public ShapedRecipe NautilusRecipe(){
+        ItemStack result = new ItemStack(Material.NAUTILUS_SHELL);
+
+        NamespacedKey key = new NamespacedKey(this, "Nautilus_Shell");
+        ShapedRecipe recipe = new ShapedRecipe(key, result);
+        recipe.shape("RBG" , "B B" , "GBR");
+        recipe.setIngredient('B', Material.BONE_MEAL);
+        recipe.setIngredient('R', Material.BLAZE_POWDER);
+        recipe.setIngredient('G', Material.GUNPOWDER);
+
+        return recipe;
+    }
+/*
+    public ShapelessRecipe DiamondShapelessElytra(){
+        
+        ItemStack result = new ItemStack(Material.ELYTRA);
+        
+        result.setItemMeta(createArmorMeta(result, "Diamond Elytra", 3, 0, 528)); 
+
+        NamespacedKey key = new NamespacedKey(this, "Diamond_Armored_Elytra");
+
+        ShapelessRecipe recipe = new ShapelessRecipe(key, result);
+
+        recipe.addIngredient(Material.ELYTRA);
+        recipe.addIngredient(Material.DIAMOND_CHESTPLATE);
+
+        return recipe;
+    } //*/
 }
---
+++ MultiArmor/multiarmor/src/main/me/excelsies/multiarmor/OnSmithingListener.java
@@ -0,0 +1,80 @@
+package me.excelsies.multiarmor;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Material;
+
+import org.bukkit.ChatColor;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.attribute.AttributeModifier.Operation;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareSmithingEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class OnSmithingListener implements Listener {
+    
+    @EventHandler
+    public void onSmithingListerner(PrepareSmithingEvent event){
+
+        if(event.getResult().getType() == Material.ELYTRA){
+            ItemStack result = new ItemStack(Material.ELYTRA);
+
+            if(event.getInventory().getItem(0).getType() ==  Material.LEATHER_CHESTPLATE){
+                result.setItemMeta(createArmorMeta(result, "Leather Elytra", 3, 0, 80));
+                event.setResult(result);
+            }
+            else if(event.getInventory().getItem(0).getType() ==  Material.CHAINMAIL_CHESTPLATE){
+                result.setItemMeta(createArmorMeta(result, "Chainmail Elytra", 5, 0, 240));
+                event.setResult(result);
+            }
+            else if(event.getInventory().getItem(0).getType() ==  Material.IRON_CHESTPLATE){
+                result.setItemMeta(createArmorMeta(result, "Iron Elytra", 6, 0, 240));
+                event.setResult(result);
+            }
+            else if(event.getInventory().getItem(0).getType() ==  Material.GOLDEN_CHESTPLATE){
+                result.setItemMeta(createArmorMeta(result, "Golden Elytra", 5, 0, 112));
+                event.setResult(result);
+            }
+            else if(event.getInventory().getItem(0).getType() ==  Material.DIAMOND_CHESTPLATE){
+                result.setItemMeta(createArmorMeta(result, "Diamond Elytra", 8, 2, 528));
+                event.setResult(result);
+            }
+            else if(event.getInventory().getItem(0).getType() ==  Material.NETHERITE_CHESTPLATE){
+                ItemMeta meta = createArmorMeta(result, "Netherite Elytra", 8, 3, 592);
+                meta.addAttributeModifier(Attribute.GENERIC_KNOCKBACK_RESISTANCE, new AttributeModifier(UUID.randomUUID(), "generic.knockback_resistence", 1, Operation.ADD_NUMBER, EquipmentSlot.CHEST));
+                result.setItemMeta(meta);
+                event.setResult(result);
+            }
+        }
+        
+    }
+
+    private ItemMeta createArmorMeta(ItemStack item, String name, double armor, double toughness, int health) {
+
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(ChatColor.DARK_PURPLE + name);
+
+        List<String> lore = new LinkedList<String>();
+        lore.add(ChatColor.BLUE + "An Elytra that has");
+        lore.add(ChatColor.BLUE + "been fused with armor");
+
+        meta.setLore(lore);
+        
+        AttributeModifier armorModifier = new AttributeModifier(UUID.randomUUID(), "generic.armor", armor, Operation.ADD_NUMBER, EquipmentSlot.CHEST);
+        AttributeModifier toughnessModifier = new AttributeModifier(UUID.randomUUID(), "generic.armor_toughness", toughness, Operation.ADD_NUMBER, EquipmentSlot.CHEST);
+        ((Damageable) meta).setDamage(-(health + 432));
+        
+        meta.addAttributeModifier(Attribute.GENERIC_ARMOR, armorModifier);
+        meta.addAttributeModifier(Attribute.GENERIC_ARMOR_TOUGHNESS, toughnessModifier);
+        
+        return meta;
+    }
+    
+}
